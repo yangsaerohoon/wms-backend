43178bbc2a37a5a6537245527c7f88f07b416f89
yangsaerohoon <saerohoon.yang@musinsalogistics.co.kr>
2023-02-19 22:04:19 +0900

    controller 샘플 수정

--- external-api/src/main/java/com/musinsalogistics/wms/api/InboundController.java
+++ external-api/src/main/java/com/musinsalogistics/wms/api/InboundController.java
@@ -1,7 +1,28 @@
 package com.musinsalogistics.wms.api;
 
+import com.musinsalogistics.wms.dto.CreateInboundScheduleRequest;
+import com.musinsalogistics.wms.dto.InboundScheduleDto;
+import com.musinsalogistics.wms.service.InboundScheduleServiceFacade;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InboundController {
+
+    private final InboundScheduleServiceFacade inboundScheduleServiceFacade;
+
+    public InboundController(InboundScheduleServiceFacade inboundScheduleServiceFacade) {
+        this.inboundScheduleServiceFacade = inboundScheduleServiceFacade;
+    }
+
+    @PostMapping("/v1/api/inbound-schedules")
+    ResponseEntity<InboundScheduleDto> createInboundSchedule(@RequestBody CreateInboundScheduleRequest request) {
+        InboundScheduleDto result =
+                inboundScheduleServiceFacade.createInboundSchedule(request);
+
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
 }
